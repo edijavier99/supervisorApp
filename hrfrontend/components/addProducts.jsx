0dcba4757495ce9b42a2fac6e48e6ea11caabedd
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-08-11 21:24:12 +0100

    delivery screen / divided in categories, each category has the posibility to add a new product with a modal where name and cuantity should be added. Then, once click create new order, the user will be able to add product cuantity based on how many should have and how many has at the moment of creating the order

---
+++ hrfrontend/components/addProducts.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+import { View, Text, Modal, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; // Solo importamos el ícono necesario
+
+export const AddProduct = ({ categoryId, categoryTitle, onAddProduct }) => {
+  const [visibleModal, setVisibleModal] = useState(false);
+  const [productName, setProductName] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const handleAddProduct = () => {
+    onAddProduct(categoryId, productName, quantity);
+    setVisibleModal(false);
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={() => setVisibleModal(true)}>
+        <MaterialIcons name="add" size={24} color="green" />
+      </TouchableOpacity>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={visibleModal}
+        onRequestClose={() => setVisibleModal(false)} // Cambiado a false
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Añadir {categoryTitle}</Text>
+
+            {/* Formulario para añadir nuevo producto */}
+            <View style={styles.formAddProduct}>
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>Nombre</Text>
+                <TextInput
+                  style={styles.input}
+                  value={productName}
+                  onChangeText={setProductName}
+                  placeholder="Ingrese el nombre del producto"
+                />
+              </View>
+
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>Cant.</Text>
+                <TextInput
+                  style={styles.input}
+                  value={quantity}
+                  onChangeText={setQuantity}
+                  placeholder="0"
+                  keyboardType="numeric"
+                />
+              </View>
+            </View>
+
+            <TouchableOpacity
+              style={[styles.button, styles.buttonAdd]}
+              onPress={handleAddProduct}
+            >
+              <Text style={styles.textStyle}>Añadir Producto</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setVisibleModal(false)}
+            >
+              <Text style={styles.textStyle}>Cerrar</Text>
+            </TouchableOpacity>
+            
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semi-transparente
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "95%",
+  },
+  inputContainer: {
+    marginBottom: 15,
+  },
+  formAddProduct: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginVertical: 5,
+    width: '100%',
+  },
+  buttonAdd: {
+    backgroundColor: '#4CAF50',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
